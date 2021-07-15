1f1fbf8773c2a4002bc7431d1b7adaaae5a20e7c
teruki.yonashiro <teruki.yonahiro31@dcom-net.jp>
2021-07-15 15:53:22 +0900

    変更

--- Dentaku/Dentaku/Program.cs
+++ Dentaku/Dentaku/Program.cs
@@ -26,55 +26,65 @@ namespace Dentaku
             string[] inputArray = formula.Split(' ');
             Check.InputCheck(inputArray[0]);
 
-            if (inputArray.Length > 2)
-            {
-                Check.OperaterCheck(inputArray[1]);
-                Check.InputCheck(inputArray[2]);
-            }
-            else if (inputArray.Length < 3)
-            {
-                //配列の追加
-                //入力値読み取り
-                Array.Resize(ref inputArray, inputArray.Length + 1);
-                inputArray[inputArray.Length - 1] = Console.ReadLine();
-                Check.OperaterCheck(inputArray[1]);
-                Array.Resize(ref inputArray, inputArray.Length + 1);
-                inputArray[inputArray.Length - 1] = Console.ReadLine();
-                Check.InputCheck(inputArray[2]);
-            }
-
             decimal inputNumber1 = decimal.Parse(inputArray[0]);
-            decimal inputNumber2 = decimal.Parse(inputArray[2]);
-            decimal result = 0;
-            Console.Write("計算結果：");
-
-            switch (inputArray[1])
+            while (true)
             {
-                case Operator.ADD:
-                    result = decimal.Add(inputNumber1, inputNumber2);
-                    Console.WriteLine(result);
-                    break;
-                case Operator.SUB:
-                    result = decimal.Subtract(inputNumber1, inputNumber2);
-                    Console.WriteLine(result);
-                    break;
-                case Operator.MUL:
-                    result = decimal.Multiply(inputNumber1, inputNumber2);
-                    Console.WriteLine(result);
-                    break;
-                case Operator.DIV:
-                    Check.ZeroDiv(inputNumber2);
-                    result = decimal.Divide(inputNumber1, inputNumber2);
-                    Console.WriteLine(result);
-                    break;
-                default:
-                    break;
-            }
+                
 
-            //decimal型の桁を取得する為変換処理
-            int ketasyori = decimal.ToInt32(result);
-            Console.WriteLine("整数値の桁数:" + ketasyori.ToString().Length);
-            Console.Read();
+                    if (inputArray.Length > 3)
+                    {
+                        Array.Resize(ref inputArray, inputArray.Length + 1);
+                        inputArray[inputArray.Length - 1] = Console.ReadLine();
+                        Console.Write("計算結果：");
+                    }
+                    else if (inputArray.Length < 3)
+                    {
+                        //配列の追加
+                        //入力値読み取り
+
+                        Array.Resize(ref inputArray, inputArray.Length + 1);
+                        inputArray[inputArray.Length - 1] = Console.ReadLine();
+                        Check.OperaterCheck(inputArray[1]);
+                        Array.Resize(ref inputArray, inputArray.Length + 1);
+                        inputArray[inputArray.Length - 1] = Console.ReadLine();
+                        Check.InputCheck(inputArray[2]);
+                    }
+
+
+
+                    decimal result = 0;
+                    Console.Write("計算結果：");
+
+                    if (inputArray.Length >= 3 && inputArray.Length % 3 == 0)
+                    {
+                        decimal inputNumber2 = decimal.Parse(inputArray[inputArray.Length - 1]);
+                        switch (inputArray[inputArray.Length - 2])
+                        {
+                            case Operator.ADD:
+                                inputNumber1 = decimal.Add(inputNumber1, inputNumber2);
+                                Console.WriteLine(inputNumber1);
+                                break;
+                            case Operator.SUB:
+                                result = decimal.Subtract(inputNumber1, inputNumber2);
+                                Console.WriteLine(result);
+                                break;
+                            case Operator.MUL:
+                                result = decimal.Multiply(inputNumber1, inputNumber2);
+                                Console.WriteLine(result);
+                                break;
+                            case Operator.DIV:
+                                Check.ZeroDiv(inputNumber2);
+                                result = decimal.Divide(inputNumber1, inputNumber2);
+                                Console.WriteLine(result);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    //decimal型の桁を取得する為変換処理
+                    Console.Read();
+                }
+            }
         }
     }
-}
+
