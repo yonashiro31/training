2ac788cc6d303431e9a583f184e645da23d6eb96
teruki.yonashiro <teruki.yonahiro31@dcom-net.jp>
2021-08-02 09:33:45 +0900

    追加

---
+++ Dentaku/Dentaku/MessageJudge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dentaku
+{
+    /// <summary>
+    /// 判定時利用する数値の定数定義を行うクラス
+    /// </summary>
+    /// <remarks>
+    /// 必要に応じて別クラスに渡し利用する
+    /// </remarks>
+    public class MessageJudge
+    {
+        /// <summary>演算子判定時の定数</summary>
+        /// <remarks>演算子判定時の定数</remarks>
+        public const int OPERATORJUDGE = 1;
+        /// <summary>0割判定時の定数</summary>
+        /// <remarks>0割判定時の定数</remarks>
+        public const int DIVJUDGE = 2;
+        /// <summary>数値入力判定時の定数</summary>
+        /// <remarks>数値入力判定時の定数</remarks>
+        public const int NUMBERJUDGE = 3;
+        /// <summary>未入力判定時の定数</summary>
+        /// <remarks>未入力判定時の定数</remarks>
+        public const int NULLJUDGE = 4;
+        /// <summary>プログラム終了判定時定数</summary>
+        /// <remarks>プログラム終了判定時定数</remarks>
+        public const int BREAKJUDGE = 5;
+    }
+}
+
